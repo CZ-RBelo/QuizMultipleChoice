a54a979458680211393cd0c49f27978f0db0f0cd
CZ-RBelo <jr.belo@hotmail.com>
2022-12-09 17:53:44 +0000

    Add scores js file

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -13,3 +13,44 @@ questionChoices.innerHTML = "";
 questionTitle.textContent = quizQuestions[currentQuestionIndex].questTitle;
 
 */
+
+
+var userInitials = "";
+var userResult = 0; 
+
+function displayHS(userInitials, userResult)
+{
+
+    var x = document.getElementById("myText").value;
+
+
+    if (initialsInp.value.length > 0) {
+
+        console.log(initialsInp.value);
+        console.log(numCorrects);
+
+        //window.location.href = "highscores.html";
+    };
+    */
+
+
+console.log(userInitials);
+console.log(userResult);
+
+};
+
+/*
+let storeInitials = (...input) => {
+    let data = JSON.stringify({ "name":input[0], "score":input[1]})
+    localStorage.setItem("object", data)
+}
+storeInitials(initialBox.value, userScore);
+
+var playAgain = document.createElement("button");
+playAgain.textContent= "Play Again!";
+blank.appendChild(playAgain);
+
+playAgain.addEventListener("click", () => {
+    location.reload();
+})
+*/

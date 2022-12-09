18aa2060a3f8df000ace1b4fec873564e53aa317
CZ-RBelo <jr.belo@hotmail.com>
2022-12-09 11:58:35 +0000

    Update the Questions file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,37 +1,36 @@
 
 // Fifa World Cup Quiz Challenge Questions & Answers
 var quizQuestions =[
-    /* 
-    {
+        {
         questTitle: "Who won the 1982 Fifa World Cup Spain?",
         answerChoices: ['West Germany', 'France', 'Poland', 'Italy'],
-        correctAnswer: 3 // correctAnswer:"Italy"
+        correctAnswer:"Italy"
     },
     {
         questTitle: "Who won the 1986 Fifa World Cup Mexico?",
         answerChoices: ['Argentina', 'Belgium', 'France', 'West Germany'],
-        correctAnswer: 0 // correctAnswer:"Argentina"
+        correctAnswer:"Argentina"
     },
     {
         questTitle: "Who won the 1990 Fifa World Cup Italy?",
         answerChoices: ['West Germany ', 'England', 'Italy', 'Argentina'],
-        correctAnswer: 0 // correctAnswer:"West Germany"
+        correctAnswer:"West Germany"
     },
     {
         questTitle: "Who won the 1994 Fifa World Cup United States?",
         answerChoices: ['Sweden', 'Brazil', 'Bulgaria', 'Italy'],
-        correctAnswer: 1 // correctAnswer:"Brazil"
+        correctAnswer:"Brazil"
     },
     {
         questTitle: "Who won the 1996 Fifa World Cup France?",
         answerChoices: ['France', 'Croatia', 'Brazil', 'Netherlands'],
-        correctAnswer: 0 // correctAnswer:"France"
+        correctAnswer:"France"
     },
     {
         questTitle: "Who won the 2002 Fifa World Cup Japan , South Korea?",
         answerChoices: ['Brazil', 'Turkey', 'Germany', 'South Korea'],
         correctAnswer:"Brazil"
-    },*/
+    },
     {
         questTitle: "Who won the 2006 Fifa World Cup Germany?",
         answerChoices: ['Portugal', 'France', 'Germany', 'Italy'],
@@ -41,7 +40,7 @@ var quizQuestions =[
         questTitle: "Who won the 2010 Fifa World Cup South Africa?",
         answerChoices: ['Germany', 'Spain', 'Uruguay', 'Netherlands'],
         correctAnswer:"Spain"
-    }/*,
+    },
     {
         questTitle: "Who won the 2014 Fifa World Cup Brazil?",
         answerChoices: ['Netherlands', 'Argentina', 'Brazil', 'Germany'],
@@ -52,5 +51,4 @@ var quizQuestions =[
         answerChoices: ['Croatia', 'England', 'France', 'Belgium'],
         correctAnswer:"France"
     }
-    */
 ]
